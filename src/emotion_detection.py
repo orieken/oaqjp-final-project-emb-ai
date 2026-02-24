1047fa3c7107ae9e130eb0a103d988a337730c65
Oscar Rieken <oriekenjr@gmail.com>
2026-02-23 23:05:56 -0600

    :bug: (emotion_detection) moved to src

--- src/emotion_detection.py
+++ src/emotion_detection.py
@@ -17,13 +17,14 @@ def emotion_detector(text_to_analyse):
   payload = { "raw_document": { "text": text_to_analyse } }
   response = requests.post(url, headers=headers, json=payload)
   json = response.json()
-  anger_score = json['emotionPredictions'][0]['emotions']['anger']
-  disgust_score = json['emotionPredictions'][0]['emotions']['disgust']
-  fear_score = json['emotionPredictions'][0]['emotions']['fear']
-  joy_score = json['emotionPredictions'][0]['emotions']['joy']
-  sadness_score = json['emotionPredictions'][0]['emotions']['sadness']
 
-  emotions = json['emotionPredictions'][0]['emotions']
+  emotions = json['emotionPredictions'][0]['emotion']
+  anger_score = emotions['anger']
+  disgust_score = emotions['disgust']
+  fear_score = emotions['fear']
+  joy_score = emotions['joy']
+  sadness_score = emotions['sadness']
+
   high_score = max(emotions, key=emotions.get)
 
   return {
@@ -32,10 +33,7 @@ def emotion_detector(text_to_analyse):
     'fear': fear_score,
     'joy': joy_score,
     'sadness': sadness_score,
-    'dominant_emotion': high_score
-  }
+    'dominant_emotion': high_score,
+    }
     
 # {'emotionPredictions': [{'emotion': {'anger': 0.01364663, 'disgust': 0.0017160787, 'fear': 0.008986978, 'joy': 0.9719017, 'sadness': 0.055187024}, 'target': '', 'emotionMentions': [{'span': {'begin': 0, 'end': 27, 'text': 'I love this new technology.'}, 'emotion': {'anger': 0.01364663, 'disgust': 0.0017160787, 'fear': 0.008986978, 'joy': 0.9719017, 'sadness': 0.055187024}}]}], 'producerId': {'name': 'Ensemble Aggregated Emotion Workflow', 'version': '0.0.1'}}    # 
-
-# r = emotion_detector("I love this new technology.")
-# print(r)
